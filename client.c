9856631007fce538f065f7872fcd227eca0b51a3
Howard Ho <howard036060006@gmail.com>
2018-05-14 23:52:58 +0800

    client can chat online and server can limit connections

---
+++ client.c
@@ -0,0 +1,91 @@
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+
+typedef struct {
+    char name[10];
+    char msg[1024];
+} Msg;
+
+int server_sock;
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+      puts("-----Bye Bye-----");
+      close(server_sock);
+      exit(0);
+  }
+}
+
+void get_msg(void *arg){
+    Msg msg;
+    int s_len;
+
+    printf("Enter your name : ");
+    scanf("%s" , msg.name);
+
+    puts("-----Let's Talk-----");
+
+    while(1){
+        fgets(msg.msg, 1024, stdin);
+
+        s_len = strlen(msg.msg);
+        msg.msg[s_len - 1] = '\0';
+
+        if(s_len > 1){
+            send(server_sock, &msg, sizeof(Msg), 0);
+        }
+    }
+}
+
+int main(int argc , char *argv[]){
+    struct sockaddr_in server;
+    pthread_t new_thread;
+    Msg rcv_msg;
+
+    if (signal(SIGINT, sig_handler) == SIG_ERR) {
+        printf("\ncan't catch SIGINT\n");
+    }
+
+    //Create socket
+    server_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (server_sock == -1)
+    {
+        perror("socker");
+        exit(1);
+    }
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5000);
+
+    //Connect to remote server
+    if (connect(server_sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect");
+        return 1;
+    }
+
+    puts("-----Chatroom Connected-----");
+
+    //keep communicatin with server
+    pthread_create(&new_thread, NULL, get_msg, (void *)"*");
+
+    // keep receiving msg from server
+    while(1){
+        if(recv(server_sock , &rcv_msg , sizeof(Msg) , 0) < 0){
+            perror("recv");
+            break;
+        }
+        printf("%s: %s\n", rcv_msg.name, rcv_msg.msg);
+    }
+
+    close(server_sock);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,253 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>    //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+#include <signal.h>
+#include <pthread.h>
+
+// max connections amount
+#define MAX_CONN 3
+
+// client msg format, send between clients
+typedef struct {
+    char name[10];
+    char msg[1024];
+} Msg;
+
+// server maintain client's connections table
+typedef struct {
+    int client_sock;
+    char ip[16];
+    int port;
+} Connection;
+
+// init connection table
+Connection * con_table[MAX_CONN];
+
+// server socker descriptor
+int server_sock;
+
+int con_count = 0;
+
+// handle SIGINT keyboard interrupt
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+      printf("- Close socket -");
+      close(server_sock);
+      exit(1);
+  }
+}
+
+int hash(int client_sock){
+    return client_sock % MAX_CONN;
+}
+
+void init_table(void){
+    for(int i = 0; i < MAX_CONN; i++){
+        con_table[i] = NULL;
+    }
+}
+
+// insert table
+void insert(int client_sock, char *ip, int port){
+    int hash_index = hash(client_sock);
+
+    Connection *this = (Connection *) malloc(sizeof(Connection));
+
+    this -> client_sock = client_sock;
+    strcpy(this->ip, ip);
+    this -> port = port;
+
+    while(con_table[hash_index] != NULL){
+        hash_index++;
+        hash_index %= MAX_CONN;
+    }
+
+    con_table[hash_index] = this;
+}
+
+Connection * search(int client_sock){
+    int search_count = 0, hash_index = hash(client_sock);
+
+    while(con_table[hash_index] != NULL){
+        if(con_table[hash_index] -> client_sock == client_sock){
+            return con_table[hash_index];
+        }
+
+        hash_index++;
+        hash_index %= MAX_CONN;
+
+        search_count++;
+        if(search_count >= MAX_CONN){
+            return NULL;
+        }
+    }
+
+    return NULL;
+}
+
+void delete(int client_sock){
+    int hash_index = hash(client_sock);
+
+    while(con_table[hash_index] != NULL){
+        if(con_table[hash_index] -> client_sock == client_sock){
+            con_table[hash_index] = NULL;
+            free(con_table[hash_index]);
+        }
+
+        hash_index++;
+        hash_index %= MAX_CONN;
+    }
+}
+
+void show(void){
+    puts("-----Connections Pool-----");
+    for(int i = 0; i < MAX_CONN; i++){
+        if(con_table[i] != NULL){
+            printf("%s:%d\n", con_table[i] -> ip, con_table[i] -> port);
+        } else {
+            puts("NULL");
+        }
+    }
+}
+
+
+// serve every connection
+void accept_client(void *arg){
+    //accept connection from an incoming client
+    int client_sock, this_sock, read_size, c = sizeof(struct sockaddr_in);
+    struct sockaddr_in client;
+    pthread_t new_thread, myid = pthread_self();
+    Msg msg;
+
+    //*********connections limited***********//
+    while(1){
+        // printf("-----con_count: %d-----\n", con_count);
+        if(con_count < MAX_CONN) break;
+        sleep(1);
+    }
+    //***************end*********************//
+
+    //*************accept*********************//
+    client_sock = accept(server_sock, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0){
+        perror("accept");
+        pthread_exit(NULL);
+    }
+    //**************end***********************//
+
+    // create thread to accept
+    if(con_count < MAX_CONN - 1){
+        pthread_create(&new_thread, NULL, accept_client, (void *) "*");
+    }
+
+    //*********client info*********************//
+    char client_ip[16];
+    int client_port;
+
+    strcpy(client_ip, inet_ntoa(client.sin_addr));
+    client_port = ntohs(client.sin_port);
+
+    printf("- Accept connection from %s:%d -\n", client_ip, client_port);
+    //********end******************************//
+
+    //****store client info to client_table****//
+    insert(client_sock, client_ip, client_port);
+    show();
+    con_count++;
+    //******end********************************//
+
+    //****Receive a message from client********//
+    while((read_size = recv(client_sock, &msg, sizeof(Msg), 0)) > 0){
+        // send to every one in client_table
+        for(int i = 0; i < MAX_CONN; i++){
+            if(con_table[i] != NULL && (con_table[i] -> client_sock) != client_sock){
+                write(con_table[i] -> client_sock, &msg, sizeof(msg));
+            }
+        }
+    }
+
+    if(read_size == 0){
+        printf("- %s:%d disconnect -\n", client_ip, client_port);
+        fflush(stdout);
+
+        // clear this connection
+        delete(client_sock);
+        show();
+
+        // time to server another connection when decreace from MAX_CONN
+        if(con_count == MAX_CONN){
+            pthread_create(&new_thread, NULL, accept_client, (void *) "*");
+        }
+        con_count--;
+
+        // terminate thread
+        pthread_exit(NULL);
+    }
+    else if(read_size == -1){
+        perror("recv");
+
+        // clear this connection
+        delete(client_sock);
+        show();
+        con_count--;
+
+        // terminate thread
+        pthread_exit(NULL);
+    }
+    //**********end****************************//
+
+    return;
+}
+
+int main(int argc , char *argv[])
+{
+    struct sockaddr_in server;
+    pthread_t new_thread;
+
+    // init connection table
+    init_table();
+
+    //Create socket
+    server_sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (server_sock == -1)
+    {
+        perror("socker");
+        exit(1);
+    }
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(5000);
+
+    //Bind
+    if(bind(server_sock,(struct sockaddr *)&server , sizeof(server)) < 0){
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+
+    //Listen
+    listen(server_sock , 3);
+
+    if (signal(SIGINT, sig_handler) == SIG_ERR) {
+        printf("\ncan't catch SIGINT\n");
+    }
+
+    //Accept and incoming connection
+    puts("- Waiting for incoming connections... -");
+
+    //accept connection from an incoming client
+    pthread_create(&new_thread, NULL, accept_client, (void *) "*");
+
+    while(1){
+        // sleep 200 milesecond
+        usleep(200*1000);
+    }
+
+    return 0;
+}
